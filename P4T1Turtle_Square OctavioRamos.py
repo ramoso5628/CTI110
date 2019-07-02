6d5036d6a9f5d5188a063f45aabd75e5e4171b07
ramoso5628 <51935862+ramoso5628@users.noreply.github.com>
2019-07-01 20:58:02 -0400

    Add files via upload
    # Turtle Square
    # 1 July 2019
    # Octavio Ramos
    # P4T1 Turtle Square

---
+++ P4T1Turtle_Square OctavioRamos.py
@@ -0,0 +1,23 @@
+# Turtle Square
+# 1 July 2019
+# Octavio Ramos
+# P4T1 Turtle Square
+
+
+import turtle             # Allows us to use turtles
+win = turtle.Screen()      # Creates a playground for turtles
+t = turtle.Turtle()    # Create a turtle, assign to alex
+
+
+#commands from here to the last line can be replaced 
+
+t.forward(50)          # Tell alex to move forward by 50 units
+t.left(90)             # Tell alex to turn by 90 degrees
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+
+# end commands
+win.mainloop()             # Wait for user to close window
